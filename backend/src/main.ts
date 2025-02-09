322fc43b738d7b80373ac2139fa298fad7e180ce
edgar <a.edgar2209@gmail.com>
2025-02-09 07:23:30 -0500

    feat: cors integration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Reemplaza con la URL de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Permitir cookies y encabezados de autenticación
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
